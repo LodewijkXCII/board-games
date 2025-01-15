7ab1fc32e51ec5ce266e175875dfa0d378485c55
LodewijkXCII <61330483+LodewijkXCII@users.noreply.github.com>
2025-01-15 10:53:26 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  // output: "server",
   server: { host: true },
   integrations: [vue(), db()],
   adapter: netlify(),
