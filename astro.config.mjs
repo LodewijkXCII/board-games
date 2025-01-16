4afa9cb155fece8589f8f46c6b024ed6daeed49a
LodewijkXCII <loek@zweersdesign.com>
2025-01-16 20:47:18 +0100

    switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
 
 import db from "@astrojs/db";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   server: { host: true },
   integrations: [db()],
-  adapter: netlify({ edgeMiddleware: true }),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
